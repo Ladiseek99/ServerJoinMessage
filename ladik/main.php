7bffd2d288fffb922daa36a22ba48d38e63e6f81
Ladiseek99 <ladikdev@seznam.cz>
2017-04-25 01:03:13 +0200

    Create main.php

---
+++ ladik/main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace ladik;
+
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\command\Command;
+use pocketmine\Server;
+use pocketmine\command\CommandSender;
+use pocketmine\utils\TextFormat as L;
+use pocketmine\utils\TextFormat;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\title\Sign;
+
+class main extends PluginBase implements Listener{
+    
+      public function onEnable() {
+          $this->getServer()->getLogger()->info(self::getPrefix()."Â§aAktivovan!");
+          $this->getServer()->getPluginManager()->registerEvents($this, $this);
+     }
+      
+      public static function getPrefix(){
+        return "Â§lÂ§4[Â§rÂ§6ServerJoinMessageÂ§lÂ§4] Â§rÂ§f";
+    }     
+     public function onJoin(PlayerJoinEvent $event){
+      	$player = $event->getPlayer();
+		$name = $player->getName();
+     $player->sendPopup("Â§1 Welcome to server Â§4 $name");
+     $player->sendMessage("Â§a---------------------- ");
+     $player->sendMessage("Â§e Hello ");
+     $player->sendMessage("Â§e Enjoy the game Â§4$name");
+     $player->sendMessage("Â§a----------------------");
+    }             
+ } 
